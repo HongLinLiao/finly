85b82c7d773d13da2991d69bdc17cdd128cb6ac9
HongLinLiao <honglin0822@outlook.com>
2026-04-03 16:51:01 +0800

    🧱 Import sort eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,39 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+        },
+      ],
+    },
+  },
   prettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
